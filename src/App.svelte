8cd8ee081642aeb3e397b71cf2e1a95b36d2510d
Jesus Marron <jesusmarron7@gmail.com>
2023-05-05 18:34:41 -0500

    created base components for feedback cards

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,38 @@
 <script>
+  import FeedbackList from "./components/FeedbackList.svelte";
+  import FeedbackStats from "./components/FeedbackStats.svelte";
+
+  let feedback = [
+    {
+      id: 1,
+      rating: 10,
+      text: "Lorem ea occaecat commodo laboris quis cillum amet consequat. Cillum sit consequat dolore voluptate cillum pariatur ut proident cupidatat. Consectetur reprehenderit ullamco velit pariatur.",
+    },
+    {
+      id: 2,
+      rating: 5,
+      text: "Lorem ea occaecat commodo laboris quis cillum amet consequat. Cillum sit consequat dolore voluptate cillum pariatur ut proident cupidatat. Consectetur reprehenderit ullamco velit pariatur.",
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: "Lorem ea occaecat commodo laboris quis cillum amet consequat. Cillum sit consequat dolore voluptate cillum pariatur ut proident cupidatat. Consectetur reprehenderit ullamco velit pariatur.",
+    },
+  ];
+
+  $: count = feedback.length;
+  $: average =
+    feedback.reduce((a, { rating }) => a + rating, 0) / feedback.length;
+  const deleteFeedback = (e) => {
+    const itemId = e.detail;
+
+    feedback = feedback.filter((item) => item.id != itemId);
+  };
 </script>
 
-<main>
-  <h1>Hello</h1>
+<main class="container">
+  <FeedbackStats {count} {average} />
+  <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
 
 <style>
